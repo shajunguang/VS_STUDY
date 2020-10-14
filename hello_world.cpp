777f369ad3ea3caee0d3f3543666bd4a0497fce9
shajunguang <41252062+shajunguang@users.noreply.github.com>
2020-10-14 10:45:29 +0800

    123

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main(){
    char name[100];
-   printf("What is your name?\n");
-   scanf("%s",name);
-   printf("Hello,%s,nice to meet you!\n",name);
+   //printf("What is your name?\n");
+   //scanf("%s",name);
+   //printf("Hello,%s,nice to meet you!\n",name);
+   char ch[2][5]={"1234","5678"},(*p)[5];
+   int i,j,s=0;
+   p=ch;
+   for(i=0;i<2;i++)
+      for(j=0;*(*(p+i)+j)!='\0';j++)
+         s+=p[i][j]-'0';
+   printf("%d\n",s);
 }
